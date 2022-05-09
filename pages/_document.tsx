67c3ba79c66f32315604202be608d18db03aa9ba
Adam Sobotka <vorcigernix@gmail.com>
2022-05-09 15:00:29 +0200

    first bug omg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -207,12 +207,8 @@ export default class MyDocument extends Document {
             </header>
             <div>
               <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <div className="px-4 py-6 sm:px-0">
-                  <div className="h-96 rounded-lg border-4 border-dashed border-gray-200">
-                    <Main />
-                    <NextScript />
-                  </div>
-                </div>
+                <Main />
+                <NextScript />
               </div>
             </div>
           </div>
