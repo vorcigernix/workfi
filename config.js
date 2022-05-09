5237b805e9b61699c8462f4de17df7b5c8c7310a
Adam Sobotka <vorcigernix@gmail.com>
2022-05-09 17:37:38 +0200

    smartcontract infra

---
+++ config.js
@@ -0,0 +1,4 @@
+
+  export const contractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9"
+  export const ownerAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+  
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ethers } from 'ethers'
+import { AccountContext } from '../components/context'
+import { ownerAddress } from '../config'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
