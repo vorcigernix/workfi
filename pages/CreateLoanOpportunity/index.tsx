d23c1415f6168bafb4f517ba5c75b2b860d75e02
Ramzi Khahil <ramzi.khahil@pm.me>
2022-05-11 16:33:29 +0200

    Job and Load opportunities added.

---
+++ pages/CreateLoanOpportunity/index.tsx
@@ -0,0 +1,6 @@
+import { useState } from 'react';
+import { LoanOpportunity } from '../api/data/LoanOpportunity'
+
+export default function CreateJobOpportunity() {
+  return 'Please select job, to create a loan opportunity.'
+}
