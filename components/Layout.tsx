16f84af4c02631bfb9b5dab1778532f96ecca35a
Ramzi Khahil <ramzi.khahil@pm.me>
2022-05-11 16:43:47 +0200

    Unnecessary header removed

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -202,12 +202,6 @@ export default function Layout({ children }: Props) {
         </>
       )}
     </Disclosure>
-
-    <header className="bg-white shadow">
-      <div className="mx-auto max-w-7xl py-6 px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-      </div>
-    </header>
     
     {/* End of header, start of body */}
     <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
