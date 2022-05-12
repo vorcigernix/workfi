6cd0569b8ff40dbe5ce3444cacbf4391e8be366d
Adam Sobotka <vorcigernix@gmail.com>
2022-05-12 12:25:39 +0200

    createjob hook refactor

--- components/CreateJob.tsx
+++ components/CreateJob.tsx
@@ -1,43 +1,103 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import { Bounty, Status } from '../pages/api/data/Bounty'
 
 export default function CreateJob() {
-    const [label, setLabel] = useState('');
-    const [organization, setOrganization] = useState('');
-    const [description, setDescription] = useState('');
-    const [duration, setDuration] = useState(0);
-    const [bounty, setBounty] = useState(0);
-    const [recruiter, setRecruiter] = useState('');
+  //const [label, setLabel] = useState('');
+  //const [organization, setOrganization] = useState('');
+  //const [description, setDescription] = useState('');
+  //const [duration, setDuration] = useState(0);
+  //const [bounty, setBounty] = useState(0);
+  //const [recruiter, setRecruiter] = useState('');
 
-    const bountyObj: Bounty = {
-        id:1,
-        label,
-        organization,
-        description,
-        duration,
-        bounty,
-        recruiter,
-        postDate: new Date(new Date().getTime()),
-        startDate: new Date(Date.UTC(0, 0, 0)),
-        worker: '',
-        status: Status.open
-    }
+  const [bountyData, setBountyData] = useState<Bounty>({
+    id: 1,
+    label: '',
+    organization: '',
+    description: '',
+    duration: 0,
+    bounty: 0,
+    recruiter: '',
+    postDate: new Date(new Date().getTime()),
+    startDate: new Date(Date.UTC(0, 0, 0)),
+    worker: '',
+    status: Status.open,
+  })
 
-    function handlePostJobEvent(){
-        console.log('Job is posted', bountyObj);
-    }
-          
-    return (
-      <div style={{display:'grid', gap:'15px'}}>
-        <div style={{display:'grid', gap:'15px', gridTemplateColumns:'auto 1fr'}}>
-          <label>Label</label><input type='text' value={label} onChange={e=>setLabel(e.target.value)} className='border' />
-          <label>Organization</label><input type='text' value={organization} onChange={e=>setOrganization(e.target.value)} className='border' />
-          <label>Duration (Days)</label><input type='number' value={duration} onChange={e=>setDuration(parseInt(e.target.value))} className='border' />
-          <label>Bounty (Gwei)</label><input type='number' value={bounty} onChange={e=>setBounty(parseInt(e.target.value))} className='border' />
-          <label>Recruiter</label><input type='text' value={recruiter} onChange={e=>setRecruiter(e.target.value)} className='border' />
-          <label>Description</label><textarea value={description} onChange={e=>setDescription(e.target.value)} className='border' />
-        </div>
-        <button type='button' onClick={handlePostJobEvent} className='text-white bg-blue-500 py-2 focus:outline-none hover:bg-pink-600 rounded text-lg' style={{justifySelf:'center', width:'80%'}}>Post Job</button>
+  function handlePostJobEvent() {
+    console.log('Job is posted', bountyData)
+  }
+
+  return (
+    <div style={{ display: 'grid', gap: '15px' }}>
+      <div
+        style={{
+          display: 'grid',
+          gap: '15px',
+          gridTemplateColumns: 'auto 1fr',
+        }}
+      >
+        <label>Label</label>
+        <input
+          type="text"
+          value={bountyData.label}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, label: e.target.value })
+          }
+          className="border"
+        />
+        <label>Organization</label>
+        <input
+          type="text"
+          value={bountyData.organization}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, organization: e.target.value })
+          }
+          className="border"
+        />
+        <label>Duration (Days)</label>
+        <input
+          type="number"
+          value={bountyData.duration}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, duration: parseInt(e.target.value) })
+          }
+          className="border"
+        />
+        <label>Bounty (Gwei)</label>
+        <input
+          type="number"
+          value={bountyData.bounty}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, bounty: parseInt(e.target.value) })
+          }
+          className="border"
+        />
+        <label>Recruiter</label>
+        <input
+          type="text"
+          value={bountyData.recruiter}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, recruiter: e.target.value })
+          }
+          className="border"
+        />
+        <label>Description</label>
+        <textarea
+          value={bountyData.description}
+          onChange={(e) =>
+            setBountyData({ ...bountyData, description: e.target.value })
+          }
+          className="border"
+        />
       </div>
-    )
+      <button
+        type="button"
+        onClick={handlePostJobEvent}
+        className="rounded bg-blue-500 py-2 text-lg text-white hover:bg-pink-600 focus:outline-none"
+        style={{ justifySelf: 'center', width: '80%' }}
+      >
+        Post Job
+      </button>
+    </div>
+  )
 }
