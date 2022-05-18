5688509bd4d25ce7cd3d9a23b2ee8bbad5219635
Adam Sobotka <vorcigernix@gmail.com>
2022-05-18 12:57:51 +0200

    approve dialog okay

--- components/Approve.tsx
+++ components/Approve.tsx
@@ -67,11 +67,41 @@ export function Approve({ message, maxcost, erc20, dai, lock }: Props) {
                           of token will be lock
                         </p>
                       </div>
-                      <div className="mt-3 items-center gap-2 text-sm text-gray-600 sm:mt-4 sm:flex sm:flex-col">
+                      <div className="mt-3 items-start gap-2 text-sm text-gray-600 sm:mt-4 sm:flex sm:flex-col">
                         <div>Maximum total cost: {maxcost}</div>
-                        <div>ERC 20: {erc20}</div>
-                        <div>Dai: {dai}</div>
-                        <div>Lock: {lock} days</div>
+                        <div className="flex">
+                          <img
+                            src="./ethereum.svg"
+                            alt="eth"
+                            className="mr-2 h-4 w-4"
+                          />
+                          {erc20}
+                        </div>
+                        <div className="flex">
+                          <img
+                            src="./dai.svg"
+                            alt="dai"
+                            className="mr-2 h-4 w-4"
+                          />
+                          {dai}
+                        </div>
+                        <div className='flex'>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4 mr-2"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth={2}
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                            />
+                          </svg>{' '}
+                          {lock} days
+                        </div>
                       </div>
                     </div>
                   </div>
