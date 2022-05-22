99114e53c4ae490e335c87cbe1b35ea3a014d1f6
Adam Sobotka <vorcigernix@gmail.com>
2022-05-22 16:26:30 +0200

    index fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,78 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Account } from '../components/Account'
-import { NetworkSwitcher } from '../components/NetworkSwitcher'
-import { useIsMounted } from '../components/hooks/useIsMounted'
-import { useAccount } from 'wagmi'
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Account } from '../components/Account';
+import { NetworkSwitcher } from '../components/NetworkSwitcher';
+import { useIsMounted } from '../components/hooks/useIsMounted';
+import { useAccount } from 'wagmi';
 
 const Home: NextPage = () => {
-  const isMounted = useIsMounted()
-  const { data } = useAccount()
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
-      <Head>
-        <title>WorkFi</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	const isMounted = useIsMounted();
+	const { data } = useAccount();
+	return (
+		<div className="flex min-h-screen flex-col justify-start py-2">
+			<Head>
+				<title>WorkFi</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-emerald-600" href="https://nextjs.org">
-            WorkFi
-          </a>
-        </h1>
-        <div className="my-5  grid rounded-lg bg-white py-5 px-20 shadow-lg">
-        <div className="text-xl font-bold" style={{ marginTop: '1rem' }}>
-          Easily create a sponsorship for your bounties
-        </div>
-        <div className="text-stone-500" style={{ marginBottom: '2rem' }}>
-          Ise a partner job and get your sponsorship oppurtunity running now
-        </div>
-        <div className="grid grid-cols-3 gap-6">
-          <div className="grid">
-            <span className="font-bold">1. Choose a job board</span>
-            <span className="text-stone-500">
-              WorkFi works with the most trustfull onchian job boards.
-            </span>
-            <div className="flex gap-3">
-              <img src="image 5.png" className="h-10 w-11" />
-              <img src="image 6.png" className="h-10 w-11" />
-              <img src="image 7.png" className="h-10 w-11" />
-            </div>
-          </div>
-          <div className="grid">
-            <span className="font-bold">2. Create your sponsorship</span>
-            <span className="text-stone-500">
-              Simply indicate your ERC20 tokens, it's price and the incentive
-              you're willing to give to your investors.
-            </span>
-          </div>
-          <div className="grid">
-            <span className="font-bold">3. And that's it</span>
-            <span className="text-stone-500">
-              Manage your bounties in the job board, all the payment flow with
-              your workers and your investors is automated.
-            </span>
-          </div>
-        </div>
-      </div>
-      </main>
+			<main className="container mx-auto py-24">
+				<h1 className="text-6xl font-bold">
+					Welcome to{' '}
+					<a className="text-emerald-600" href="https://workfi.com">
+						WorkFi
+					</a>
+				</h1>
+				<div className="my-5 rounded-lg bg-white py-5 shadow-lg md:my-20 md:px-20">
+					<div className="-m-4 flex flex-wrap">
+						<div className="mb-6 p-4 lg:mb-0 lg:w-1/3">
+							<div className="h-full text-center">
+								<div className="my-8 inline-flex h-16 w-16 items-center justify-center rounded-full border-2 border-gray-200 bg-emerald-100 text-3xl">
+									1
+								</div>
+								<p className="font-bold leading-relaxed">Choose a job board</p>
+								<span className="mt-6 inline-block h-1 w-10 rounded bg-emerald-500"></span>
+								<p className="my-4 leading-relaxed">WorkFi works with the most trustfull onchain job boards </p>
 
+								<div className="flex justify-center gap-3">
+									<img src="image 5.png" className="h-10 w-11" />
+									<img src="image 6.png" className="h-10 w-11" />
+									<img src="image 7.png" className="h-10 w-11" />
+								</div>
+							</div>
+						</div>
+						<div className="mb-6 p-4 lg:mb-0 lg:w-1/3">
+							<div className="h-full text-center">
+								<div className="my-8 inline-flex h-16 w-16 items-center justify-center rounded-full border-2 border-gray-200 bg-emerald-200 text-3xl">
+									2
+								</div>
+								<p className="font-bold leading-relaxed">Create a sponsorship</p>
+								<span className="mt-6 inline-block h-1 w-10 rounded bg-emerald-500"></span>
+								<p className="my-4 leading-relaxed">
+									Simply indicate your ERC20 token, its price and the incentive you’re willing to give to your
+									investors.
+								</p>
+							</div>
+						</div>
+						<div className="mb-6 p-4 lg:mb-0 lg:w-1/3">
+							<div className="h-full text-center">
+								<div className="my-8 inline-flex h-16 w-16 items-center justify-center rounded-full border-2 border-gray-200 bg-emerald-300 text-3xl">
+									3
+								</div>
+								<p className="font-bold leading-relaxed">And that's it</p>
+								<span className="mt-6 inline-block h-1 w-10 rounded bg-emerald-500"></span>
+								<p className="my-4 leading-relaxed">
+									Manage you bounty in the job board, all the payment flow with your workers and your investors is
+									automated.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</main>
+		</div>
+	);
+};
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
-    </div>
-  )
-}
-
-export default Home
+export default Home;
